29292afaad3ce526c616de3bf927f205326833b5
Rabhas <rabhas.vashisht@example.com>
2024-09-17 19:59:45 -0400

    removed basename prop in browserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ const App: React.FC = () => {
   }, []);
 
   return (
-    <Router basename="/Ram_meraki">
+    <Router>
       <div>
         <AnimatePresence mode="wait">
           {showWelcome ? (
